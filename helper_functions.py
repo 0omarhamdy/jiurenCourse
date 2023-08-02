0746c814696f09e20c613ded1b256ea002f237b9
Omar <o0omarhamdy@gmail.com>
2023-08-02 16:28:39 +0300

    calc, enc, dec

--- helper_functions.py
+++ helper_functions.py
@@ -17,3 +17,27 @@ def even_or_odd(num):
         return "even"
     else:
         return "odd"
+    
+
+def encrypt(text, key=4):
+    encrypted_text = ""
+
+    for char in text:
+        encrypted_text += chr(ord(char) + key)
+
+    return encrypted_text
+
+
+def decrypt(text, key=4):
+    decrypted_text = ""
+    
+    for char in text:
+        decrypted_text += chr(ord(char) - key)
+
+    return decrypted_text
+
+
+def calculator(num1, op, num2):
+    return eval(f"{num1} {op} {num2}")
+    # use conventioal if to make the calc if you want
+
--- jikan.py
+++ jikan.py
@@ -1,6 +1,6 @@
 from time import sleep
-from random import randint
-from helper_functions import even_or_odd, ftoc, print_sleep
+from random import randint, choice
+from helper_functions import even_or_odd, ftoc, print_sleep, calculator, encrypt, decrypt
 
 print_sleep('Welcome to Jikan')
 
@@ -36,10 +36,25 @@ if 2023 > year >= 1995:
     print_sleep(even_or_odd(num))
 
 elif  1950 >= year >= 1930:
-    print('Alan Turing!')
+    print_sleep('Alan Turing!')
+    print_sleep("let's encrypt")
+    names = ["Omar", "Shahd", "Asmaa","Sarah", "Alaa", "Ammar", "Mariam", "Gehad", "Mohamed", "Dalia", "Aya", "Yousef", "Abdo", "Ali"]
+    name = choice(names)
+    print_sleep(name)
+    enc = encrypt(name)
+    print_sleep("it becomes: {enc}")
+    print_sleep("now decrypt it!")
+    dec = decrypt(enc)
+    print_sleep(dec)
     
 elif 1870 >= year >= 1840:
-    print('Charles Babbage!')
+    print_sleep('Charles Babbage!')
+    print_sleep('Solve the math challenge!')
+    num1 = randint(0, 100)
+    num2 = randint(0, 100)
+    ops = ["+", "-", "*", "/", "**", "%"]
+    op = choice(ops)
+    print_sleep(f"{num1} {op} {num2} = {calculator(num1, op, num2)}")
 
 else:
     print('Wrong range')
